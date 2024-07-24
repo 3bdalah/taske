ec4cd2fb3e44a91b8f2c48e74e4e6787c18a502d
3bdalah <3bdallah.dev@gmail.com>
2024-07-24 23:43:15 +0300

    upload file env

---
+++ src/app/enviroments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    firebaseConfig : {
+        apiKey: "AIzaSyBq9KYuW6ystusPt7WJXoNDhupeVnpmbC4",
+        authDomain: "task-28c05.firebaseapp.com",
+        databaseURL: "https://task-28c05-default-rtdb.firebaseio.com",
+        projectId: "task-28c05",
+        storageBucket: "task-28c05.appspot.com",
+        messagingSenderId: "302511361625",
+        appId: "1:302511361625:web:b89a99574c44ea6633eb48",
+        measurementId: "G-VKMHX3JJEC"
+      }
+  };
+  
+//   const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
